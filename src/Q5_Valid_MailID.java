1771111029c27dab1ca0000312ba93e91446abab
Himanshu Sharma <hsallrounder@gmail.com>
2021-12-17 23:22:39 +0530

    For literal '.' '\\.' is used because alone '.' means any character.

--- src/Q5_Valid_MailID.java
+++ src/Q5_Valid_MailID.java
@@ -6,7 +6,7 @@ public class Q5_Valid_MailID {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         String target = sc.nextLine();
-        Pattern p = Pattern.compile("[a-zA-Z0-9_.]*[a-zA-Z]+[a-zA-Z0-9_.]*@([a-z]+.)+[a-z]+");
+        Pattern p = Pattern.compile("[a-zA-Z0-9_\\.]*[a-zA-Z]+[a-zA-Z0-9_\\.]*@([a-z]+\\.)+[a-z]+");
         Matcher m = p.matcher(target);
         if(m.find()){
             System.out.println("The email id is valid.");
